20f171eb8dfe05b9728216686a88c1a92c51c3bd
Alesya90 <alesyazhuravlik@gmail.com>
2020-11-29 16:12:22 +0500

    4. Расстояние между точками в трехмерном пространстве. [#402486]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -13,25 +13,29 @@ public class Point {
      * И это поле объекта. Оно доступно только конкретному объекту.
      */
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public double distance(Point that) {
+
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
-    /*public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        return rsl;
-    }*/
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
 
     public static void main(String[] args) {
-        /*double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        result = Point.distance(-5, 0, 0, 5);
-        System.out.println("result (-5, 0) to (0, 5) " + result);*/
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
         double dist = a.distance(b);
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -8,11 +8,6 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         int expected = 2;
-        /*int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);*/
         Point a = new Point(0, 0);
         Point b = new Point(2, 0);
         double out = a.distance(b);
@@ -22,11 +17,6 @@ public class PointTest {
     @Test
     public void when11to41then3() {
         int expected = 3;
-        /*int x1 = 1;
-        int y1 = 1;
-        int x2 = 4;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);*/
         Point a = new Point(1, 1);
         Point b = new Point(4, 1);
         double out = a.distance(b);
@@ -36,11 +26,6 @@ public class PointTest {
     @Test
     public void when40to04thensqrt32() {
         double expected = Math.sqrt(32);
-        /*int x1 = 4;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 4;
-        double out = Point.distance(x1, y1, x2, y2);*/
         Point a = new Point(4, 0);
         Point b = new Point(0, 4);
         double out = a.distance(b);
@@ -50,14 +35,36 @@ public class PointTest {
     @Test
     public void when33to68thensqrt34() {
         double expected = Math.sqrt(34);
-        /*int x1 = 3;
-        int y1 = 3;
-        int x2 = 6;
-        int y2 = 8;
-        double out = Point.distance(x1, y1, x2, y2);*/
         Point a = new Point(3, 3);
         Point b = new Point(6, 8);
         double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when330to680() {
+        double expected = Math.sqrt(34);
+        Point a = new Point(3, 3, 0);
+        Point b = new Point(6, 8, 0);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when000to200() {
+        int expected = 2;
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(2, 0, 0);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when000to111() {
+        double expected = Math.sqrt(3);
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(1, 1, 1);
+        double out = a.distance3d(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
